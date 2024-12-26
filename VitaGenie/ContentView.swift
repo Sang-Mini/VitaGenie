4bf630f4aaab5c4986408afa1918751a9e371fe7
Sang-Mini <sangmin3062@gmail.com>
2024-12-26 22:47:05 +0900

    Initial Commit

---
+++ VitaGenie/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VitaGenie
+//
+//  Created by 유상민 on 2024/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VitaGenie/VitaGenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitaGenieApp.swift
+//  VitaGenie
+//
+//  Created by 유상민 on 2024/12/26.
+//
+
+import SwiftUI
+
+@main
+struct VitaGenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
