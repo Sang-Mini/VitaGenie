21d3e5e9b0622dcf04de2321e0d114f7eb424d8d
Sang-Mini <sangmin3062@gmail.com>
2025-01-01 20:03:30 +0900

    Feat : 설문 결과를 보여주는 페이지 추가

--- VitaGenie/Views/ResultView.swift
+++ VitaGenie/Views/ResultView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ResultView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+            // 추천 결과를 표시하는 텍스트
+            Text("추천 결과를 여기에 표시합니다.")
+                .font(.title) // 텍스트 스타일 설정
+                .padding() // 텍스트 주변 여백 추가
+                .navigationTitle("추천 결과") // 네비게이션 바 제목 설정
+        }
 }
 
 struct ResultView_Previews: PreviewProvider {
