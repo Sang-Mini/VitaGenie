3fc740531c8beb84506e30ece026ed036654640f
Sang-Mini <sangmin3062@gmail.com>
2025-01-01 13:42:01 +0900

    Add File
    상수관리 파일 생성

---
+++ VitaGenie/Utilities/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  VitaGenie
+//
+//  Created by 유상민 on 2025/01/01.
+//
+
+import SwiftUI
+
+struct Constants: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Constants_Previews: PreviewProvider {
+    static var previews: some View {
+        Constants()
+    }
+}
