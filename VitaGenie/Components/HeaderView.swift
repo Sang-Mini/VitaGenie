c4c0de24a5e41545488e9385ca35efb55851eb6c
Sang-Mini <sangmin3062@gmail.com>
2025-01-07 23:00:52 +0900

    Refactor : 구분선을 그라데이션 스타일로 변경

--- VitaGenie/Components/HeaderView.swift
+++ VitaGenie/Components/HeaderView.swift
@@ -9,32 +9,33 @@ import SwiftUI
 
 struct HeaderView: View {
     var body: some View {
+        HStack {
+            Spacer() // 중앙 정렬을 위해 Spacer 추가
+            
             HStack {
-                Spacer() // 중앙 정렬을 위해 Spacer 추가
-                
-                HStack {
-                    Image(systemName: "pills.fill")// 기본 로고 (SF Symbol 사용)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
-                        .foregroundColor(Color(hex: "#fd546a")) // HEX 컬러 적용
-                    Text("VitaGenie")
-                        .font(.headline)
-                        .fontWeight(.bold)
-                        .foregroundColor(.primary)
-                }
-                
-                Spacer() // 중앙 정렬을 위해 Spacer 추가
+                Image(systemName: "pills.fill")// 기본 로고 (SF Symbol 사용)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(Color(hex: "#fd546a")) // HEX 컬러 적용
+                Text("VitaGenie")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
             }
-            .padding(.top, 8)
-            .padding(.bottom, 8)
-        
-            // 그레이 색상의 구분선 추가
-            Rectangle()
-                    .frame(height: 2)
-                    .foregroundColor(.gray.opacity(0.3))
-                    .shadow(color: .gray, radius: 5, x: 0, y: 3)
+            
+            Spacer() // 중앙 정렬을 위해 Spacer 추가
         }
+        .padding(.top, 8)
+        .padding(.bottom, 8)
+        
+        LinearGradient(
+            gradient: Gradient(colors: [.gray.opacity(0.2), .gray, .gray.opacity(0.2)]),
+            startPoint: .leading,
+            endPoint: .trailing
+        )
+        .frame(height: 2)
+    }
 }
 
 struct HeaderView_Previews: PreviewProvider {
